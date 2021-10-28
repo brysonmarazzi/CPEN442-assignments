ca11e2716883414c8c6e8882e82b0e8f315d868c
Bryson Marazzi <bryson.marazzi@gmail.com>
2021-10-27 20:51:25 -0700

    shared keyfakbf;sd

--- Assignment3/protocol.py
+++ Assignment3/protocol.py
@@ -13,7 +13,7 @@ NOT_SECURE_PREPEND = 0
 class Protocol:
     # Initializer (Called from app.py)
     # TODO: MODIFY ARGUMENTS AND LOGIC AS YOU SEEM FIT
-    def __init__(self):
+    def __init__(self, sharedKey):
         self._key = None
         self.identifier = 999 # TODO make unique identifier should be 15 bytes
         self.protocolState = 0
@@ -27,7 +27,7 @@ class Protocol:
         self.commonDH = None
         self.currentState = DEFAULT
         self.authenticate = False
-
+        self.sharedKey = sharedKey
 
     # Creating the initial message of your protocol (to be send to the other party to bootstrap the protocol)
     # TODO: IMPLEMENT THE LOGIC (MODIFY THE INPUT ARGUMENTS AS YOU SEEM FIT)
@@ -57,7 +57,7 @@ class Protocol:
         return NOT_SECURE_PREPEND.to_bytes(1, "big") + byteMsg
 
     def stringToBytes(self, str):
-        return bytes(str, 'utf-16') 
+        return bytes(str, 'utf-16')  # TODO str.encode()
 
     def intToBytes(self, num):
         return num.to_bytes(16, "big")
@@ -83,8 +83,12 @@ class Protocol:
             print("Enter Azero")
             # TODO process variables inside message Ra Rb gpmodp, g, p
             # TODO send response with rb and gpmod kab
-            # self.rSender = message[16:]
-            # self.r = self.nonce = secrets.token_urlsafe(16)
+            self.rSender = message[0:15]
+            gmodp = self.getPublicDH()
+            response = self.rSender + self.intToBytes(gmodp)
+ 
+
+            # self.
             # self.g = 11111111
             # self.p = 22222222
             # self.mydh = 33333333
@@ -130,6 +134,9 @@ class Protocol:
             print("ELSE HELO")
             return "HELOOOOO"
 
+    def getPublicDH(self):
+        return 890890809809089
+
     def isAuthenticated(self):
         return self.authenticate
 
@@ -138,6 +145,14 @@ class Protocol:
     def SetSessionKey(self, key):
         self._key = key
         pass
+
+    def decryptProtocolMsg(self, msg):
+        # self.aes.encrypt(self.key)
+        return msg
+
+    def encryptProtocolMsg(self, msg):
+        # self.aes.encrypt(self.key)
+        return msg
 
     # Encrypting messages
     # TODO: IMPLEMENT ENCRYPTION WITH THE SESSION KEY (ALSO INCLUDE ANY NECESSARY INFO IN THE ENCRYPTED MESSAGE FOR INTEGRITY PROTECTION)
